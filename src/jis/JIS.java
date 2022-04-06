d4404d05d317021c0bb98194cb2fd159953d0352
devaryanrawat <devaryanrawat29@gmail.com>
2022-04-06 12:51:41 +0530

    Commit all

---
+++ src/jis/JIS.java
@@ -0,0 +1,10 @@
+package jis;
+
+public class JIS 
+{
+    public static void main(String[] args) 
+    {
+        jis.Users user = new jis.Users();
+        user.setVisible(true);               //Displays log-in page
+    }
+}
